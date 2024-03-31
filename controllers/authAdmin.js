15b51a31b367c2050f8581c87548fbc8381553b4
unknown <malikthabit20@gmail.com>
2024-03-31 14:03:29 +0300

    v.0.6

--- controllers/authAdmin.js
+++ controllers/authAdmin.js
@@ -35,7 +35,7 @@ const login = async (req, res, next) => {
 
         const {password, ...otherDetails} = admin._doc
         res.cookie('access_token', token, {
-            
+            httpOnly: true,
             sameSite: 'none',
             secure: true
         }).status(200).json({...otherDetails, access_token:token })
